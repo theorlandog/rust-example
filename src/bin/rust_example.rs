0aa7716f7041243d19cee8447fb8d8415fb3b9ca
Jamie Orlando <jamie@theorlandog.com>
2023-01-07 21:05:50 -0500

    changing to rust_example

---
+++ src/bin/rust_example.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("I'm using the library");
+    rust_example::print_hello_world();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
-fn main() {
+pub fn print_hello_world() {
     println!("Hello, world!");
 }
